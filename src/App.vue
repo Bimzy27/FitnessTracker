0f899b3cac7b51963da431be9d7daa6df96058ae
branden.immerzeel@gmail.com <branden.immerzeel@gmail.com>
2024-09-29 21:59:30 +0930

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+import HeaderContainer from './components/HeaderContainer.vue'
+
+const name = ref('Branden')
+</script>
+
+<template>
+  <HeaderContainer/>
+  <div>
+    <label>Name: {{name}}</label>
+  </div>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
---
+++ src/components/HeaderContainer.vue
@@ -0,0 +1,33 @@
+<script>
+</script>
+
+<template>
+    <div class="header-panel">
+        <label>Fitness Tracker</label>
+        <div class="header-buttons-div">
+            <button>Profile</button>
+            <button>Excersizes</button>
+            <button>Workouts</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.header-panel {
+  height: 10vh;
+  width: 100vw;
+  background-color: #2e4a97;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+
+.header-buttons-div {
+  display: flex;
+  flex-direction: row;
+}
+</style>
